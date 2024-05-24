84606b86e28e7eaa808da7d901167997eff345b5
michael-lutz <43460304+michael-lutz@users.noreply.github.com>
2024-05-23 19:26:35 -0700

    MJX Training Implementation (#1)
    * feat: created new default humanoid environment class implementing brax and mjx
    
    * feat: ran initial training with new MJX environment
    
    * feat: added nicer training script
    
    * feat: added training config for stompy
    
    * feat: storing weight checkpoints and added play
    
    * feat: added stomppy environment
    
    * feat: added model checkpointing
    
    * feat: printing when rendering
    
    * feat: removed unnecessary model checkpointing
    
    * chore: cleaned up play script and added CPU-only rendering
    
    * chore: removed brax replication
    
    * fix: fixing stompy environment import issues and created simplified mesh
    
    * chore: sorting libraries, typing, etc
    
    * chore: cleaned up training scripts and file organization
    
    * chore: removed unused imports
    
    * fix: removing duplicate exclude definition in pyproject
    
    * fix: moved mjx_gym to ksim folder
    
    * chore: removing duplicate folders
    
    * feat: updated the readme with relevant getting-started information
    
    * chore: fixed spacing and updated setup.py
    
    * chore: updating pyproject
    
    * thing
    
    * fix some types
    
    * fix typing
    
    * chore: fixing import ordering
    
    ---------
    
    Co-authored-by: Benjamin Bolte <ben@bolte.cc>

---
+++ ksim/mjx_gym/envs/stompy_env/stompy.py
@@ -0,0 +1,165 @@
+"""Defines the Stompy MJX environment."""
+
+import os
+from typing import Unpack
+
+import jax
+import jax.numpy as jp
+import mujoco
+from brax.envs.base import PipelineEnv, State
+from brax.io import mjcf
+from brax.mjx.base import State as mjxState
+
+from ksim.mjx_gym.envs.default_humanoid_env.default_humanoid import EnvKwargs
+from ksim.mjx_gym.envs.default_humanoid_env.rewards import (
+    DEFAULT_REWARD_PARAMS,
+    RewardParams,
+    get_reward_fn,
+)
+
+
+class StompyEnv(PipelineEnv):
+    """An environment for humanoid body position, velocities, and angles."""
+
+    def __init__(
+        self,
+        reward_params: RewardParams = DEFAULT_REWARD_PARAMS,
+        terminate_when_unhealthy: bool = True,
+        reset_noise_scale: float = 1e-2,
+        exclude_current_positions_from_observation: bool = True,
+        log_reward_breakdown: bool = True,
+        **kwargs: Unpack[EnvKwargs],
+    ) -> None:
+        path = os.getenv("MODEL_DIR", "") + "/robot_simplified.xml"
+        mj_model = mujoco.MjModel.from_xml_path(path)
+        mj_model.opt.solver = mujoco.mjtSolver.mjSOL_CG
+        mj_model.opt.iterations = 6
+        mj_model.opt.ls_iterations = 6
+
+        sys = mjcf.load_model(mj_model)
+
+        physics_steps_per_control_step = 4  # Should find way to perturb this value in the future
+        kwargs["n_frames"] = kwargs.get("n_frames", physics_steps_per_control_step)
+        kwargs["backend"] = "mjx"
+
+        super().__init__(sys, **kwargs)
+
+        self._reward_params = reward_params
+        self._terminate_when_unhealthy = terminate_when_unhealthy
+        self._reset_noise_scale = reset_noise_scale
+        self._exclude_current_positions_from_observation = exclude_current_positions_from_observation
+        self._log_reward_breakdown = log_reward_breakdown
+
+        self.reward_fn = get_reward_fn(self._reward_params, self.dt, include_reward_breakdown=True)
+
+    def reset(self, rng: jp.ndarray) -> State:
+        """Resets the environment to an initial state.
+
+        Args:
+            rng: Random number generator seed.
+
+        Returns:
+            The initial state of the environment.
+        """
+        rng, rng1, rng2 = jax.random.split(rng, 3)
+
+        low, hi = -self._reset_noise_scale, self._reset_noise_scale
+        qpos = self.sys.qpos0 + jax.random.uniform(rng1, (self.sys.nq,), minval=low, maxval=hi)
+        qvel = jax.random.uniform(rng2, (self.sys.nv,), minval=low, maxval=hi)
+
+        mjx_state = self.pipeline_init(qpos, qvel)
+        assert type(mjx_state) == mjxState, f"mjx_state is of type {type(mjx_state)}"
+
+        obs = self._get_obs(mjx_state, jp.zeros(self.sys.nu))
+        reward, done, zero = jp.zeros(3)
+        metrics = {
+            "x_position": zero,
+            "y_position": zero,
+            "distance_from_origin": zero,
+            "x_velocity": zero,
+            "y_velocity": zero,
+        }
+        for key in self._reward_params.keys():
+            metrics[key] = zero
+
+        return State(mjx_state, obs, reward, done, metrics)
+
+    def step(self, state: State, action: jp.ndarray) -> State:
+        """Runs one timestep of the environment's dynamics.
+
+        Args:
+            state: The current state of the environment.
+            action: The action to take.
+
+        Returns:
+            A tuple of the next state, the reward, whether the episode has ended, and additional information.
+        """
+        mjx_state = state.pipeline_state
+        assert mjx_state, "state.pipeline_state was recorded as None"
+        # TODO: determine whether to raise an error or reset the environment
+
+        next_mjx_state = self.pipeline_step(mjx_state, action)
+
+        assert type(next_mjx_state) == mjxState, f"next_mjx_state is of type {type(next_mjx_state)}"
+        assert type(mjx_state) == mjxState, f"mjx_state is of type {type(mjx_state)}"
+        # mlutz: from what I've seen, .pipeline_state and .pipeline_step(...)
+        # actually return an brax.mjx.base.State object however, the type
+        # hinting suggests that it should return a brax.base.State object
+        # brax.mjx.base.State inherits from brax.base.State but also inherits
+        # from mjx.Data, which is needed for some rewards
+
+        obs = self._get_obs(mjx_state, action)
+        reward, is_healthy, reward_breakdown = self.reward_fn(mjx_state, action, next_mjx_state)
+
+        if self._terminate_when_unhealthy:
+            done = 1.0 - is_healthy
+        else:
+            done = jp.array(0)
+
+        state.metrics.update(
+            x_position=next_mjx_state.subtree_com[1][0],
+            y_position=next_mjx_state.subtree_com[1][1],
+            distance_from_origin=jp.linalg.norm(next_mjx_state.subtree_com[1]),
+            x_velocity=(next_mjx_state.subtree_com[1][0] - mjx_state.subtree_com[1][0]) / self.dt,
+            y_velocity=(next_mjx_state.subtree_com[1][1] - mjx_state.subtree_com[1][1]) / self.dt,
+        )
+
+        if self._log_reward_breakdown:
+            for key, val in reward_breakdown.items():
+                state.metrics[key] = val
+
+        return state.replace(pipeline_state=next_mjx_state, obs=obs, reward=reward, done=done)
+
+    def _get_obs(self, data: mjxState, action: jp.ndarray) -> jp.ndarray:
+        """Observes humanoid body position, velocities, and angles.
+
+        Args:
+            data: The current state of the environment.
+            action: The current action.
+
+        Returns:
+            Observations of the environment.
+        """
+        position = data.qpos
+        if self._exclude_current_positions_from_observation:
+            position = position[2:]
+
+        # external_contact_forces are excluded
+        return jp.concatenate(
+            [
+                position,
+                data.qvel,
+                data.cinert[1:].ravel(),
+                data.cvel[1:].ravel(),
+                data.qfrc_actuator,
+            ]
+        )
+
+
+def adhoc_test() -> None:
+    print("hello, world!")
+
+
+if __name__ == "__main__":
+    # python -m ksim.mjx_gym.envs.stompy_env.stompy
+    adhoc_test()
---
+++ ksim/mjx_gym/utils/rollouts.py
@@ -0,0 +1,127 @@
+"""Defines some utility functions for rollouts."""
+
+import logging
+from typing import Callable
+
+import jax
+import jax.numpy as jp
+import mujoco
+import numpy as np
+from brax.mjx.base import State as mjxState
+from mujoco import mjx
+from tqdm import tqdm
+
+logger = logging.getLogger(__name__)
+
+InferenceFn = Callable[[jp.ndarray, jp.ndarray], tuple[jp.ndarray, jp.ndarray]]
+
+
+def mjx_rollout(
+    env: mujoco.MjModel,
+    inference_fn: InferenceFn,
+    n_steps: int = 1000,
+    render_every: int = 2,
+    seed: int = 0,
+) -> list[mjxState]:
+    """Rollout a trajectory using MJX.
+
+    It is worth noting that env, a Brax environment, is expected to implement MJX
+    in the background. See default_humanoid_env for reference.
+
+    Args:
+        env: Brax environment
+        inference_fn: Inference function
+        n_steps: Number of steps to rollout
+        render_every: Render every nth step
+        seed: Random seed
+
+    Returns:
+        A list of pipeline states of the policy rollout
+    """
+    # print(f"Rolling out {n_steps} steps with MJX")
+    logger.info("Rolling out %d steps with MJX", n_steps)
+    reset_fn = jax.jit(env.reset)
+    step_fn = jax.jit(env.step)
+    inference_fn = jax.jit(inference_fn)
+    rng = jax.random.PRNGKey(seed)
+
+    state = reset_fn(rng)
+    rollout = [state.pipeline_state]
+    for i in tqdm(range(n_steps)):
+        act_rng, rng = jax.random.split(rng)
+        ctrl, _ = inference_fn(state.obs, act_rng)
+        state = step_fn(state, ctrl)
+        rollout.append(state.pipeline_state)
+
+        if state.done:
+            state = reset_fn(rng)
+
+    return rollout
+
+
+def render_mjx_rollout(
+    env: mujoco.MjModel,
+    inference_fn: InferenceFn,
+    n_steps: int = 1000,
+    render_every: int = 2,
+    seed: int = 0,
+) -> np.ndarray:
+    """Rollout a trajectory using MuJoCo and render it.
+
+    Args:
+        env: Brax environment
+        inference_fn: Inference function
+        n_steps: Number of steps to rollout
+        render_every: Render every nth step
+        seed: Random seed
+
+    Returns:
+        A list of renderings of the policy rollout with dimensions (T, H, W, C)
+    """
+    rollout = mjx_rollout(env, inference_fn, n_steps, render_every, seed)
+    images = env.render(rollout[::render_every], camera="side")
+
+    return np.array(images)
+
+
+def render_mujoco_rollout(
+    env: mujoco.MjModel,
+    inference_fn: InferenceFn,
+    n_steps: int = 1000,
+    render_every: int = 2,
+    seed: int = 0,
+) -> np.ndarray:
+    """Rollout a trajectory using MuJoCo.
+
+    Args:
+        env: Brax environment
+        inference_fn: Inference function
+        n_steps: Number of steps to rollout
+        render_every: Render every nth step
+        seed: Random seed
+
+    Returns:
+        A list of images of the policy rollout (T, H, W, C)
+    """
+    print(f"Rolling out {n_steps} steps with MuJoCo")
+    model = env.sys.mj_model
+    data = mujoco.MjData(model)
+    renderer = mujoco.Renderer(model)
+    ctrl = jp.zeros(model.nu)
+
+    images: list[np.ndarray] = []
+    rng = jax.random.PRNGKey(seed)
+    for step in tqdm(range(n_steps)):
+        act_rng, seed = jax.random.split(rng)
+        obs = env._get_obs(mjx.put_data(model, data), ctrl)
+        # TODO: implement methods in envs that avoid having to use mjx in a hacky way...
+        ctrl, _ = inference_fn(obs, act_rng)
+        data.ctrl = ctrl
+        for _ in range(env._n_frames):
+            mujoco.mj_step(model, data)
+
+        if step % render_every == 0:
+            renderer.update_scene(data, camera="side")
+            images.append(renderer.render())
+
+    return np.array(images)
