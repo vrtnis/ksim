f3a7dc0e40bfef1ed4d408f1b61b4b3276d59abe
vrtnis <123119434+vrtnis@users.noreply.github.com>
2024-07-09 10:42:10 -0700

    Updates

---
+++ _next/static/js/app/layout.603b738e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{2696:function(n,e,_){Promise.resolve().then(_.t.bind(_,4671,23))},4671:function(n){n.exports={style:{fontFamily:"'__Inter_71d8c9', '__Inter_Fallback_71d8c9'",fontStyle:"normal"},className:"__className_71d8c9"}}},function(n){n.O(0,[959,971,23,744],function(){return n(n.s=2696)}),_N_E=n.O()}]);
