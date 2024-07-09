d1fdb995346599b7413a34951692be78dc16218e
Allen Wu <110312634+wuallenwu@users.noreply.github.com>
2024-07-09 16:26:23 -0700

    New model training in sim (#23)
    * new stompy artifact updates
    
    * accidentally removed train.py
    
    * more updates
    
    * model update
    
    * new stompy artifact updates
    
    * accidentally removed train.py
    
    * more updates
    
    * model update
    
    * cleanup and a couple updates
    
    * gitignore mujoco_log
    
    * revert some stuff
    
    * lint
    
    * get rid of mujoco_log
    
    * more lint

--- ksim/mjx_gym/agents/ppo.py
+++ ksim/mjx_gym/agents/ppo.py
@@ -393,7 +393,6 @@ def train(
         action_repeat=action_repeat,
         key=eval_key,
     )
-
     # Run initial eval
     metrics = {}
     if process_id == 0 and num_evals > 1:
@@ -410,14 +409,12 @@ def train(
         logging.info("starting iteration %s %s", it, time.time() - xt)
 
         for _ in range(max(num_resets_per_eval, 1)):
-            # optimization
             epoch_key, local_key = jax.random.split(local_key)
             epoch_keys = jax.random.split(epoch_key, local_devices_to_use)
             (training_state, env_state, training_metrics) = training_epoch_with_timing(
                 training_state, env_state, epoch_keys
             )
             current_step = int(_unpmap(training_state.env_steps))
-
             key_envs = jax.vmap(lambda x, s: jax.random.split(x[0], s), in_axes=(0, None))(key_envs, key_envs.shape[1])
             # TODO: move extra reset logic to the AutoResetWrapper.
             env_state = reset_fn(key_envs) if num_resets_per_eval > 0 else env_state
