9188cbb7213d608adcd2d1ef315ee0a1316422f9
vrtnis <123119434+vrtnis@users.noreply.github.com>
2024-07-08 21:00:29 -0700

    Updates

---
+++ _next/static/F_CIoN-cmuIZtEgVQPGQA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/js/pages/_error.30797f45.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
